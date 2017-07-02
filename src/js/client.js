bf013cb5118cff6db34a7c040847e8d6cb51c757
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2017-07-02 20:57:47 +0200

    Add modal library. Add onclick function.

--- src/js/client.js
+++ src/js/client.js
@@ -2,6 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
+var modal = new tingle.modal({
+  footer: false,
+  stickyFooter: false,
+  closeMethods: ['overlay', 'button', 'escape'],
+  closeLabel: "Close",
+  cssClass: ['custom-class-1', 'custom-class-2'],
+  onOpen: function() {
+      console.log('modal open');
+  },
+  onClose: function() {
+      console.log('modal closed');
+  },
+  beforeClose: function() {
+      // here's goes some logic
+      // e.g. save content before closing the modal
+      return true; // close the modal
+    return false; // nothing happens
+  }
+});
+
+
 class FetchDemo extends React.Component {
   constructor(props) {
     super(props);
@@ -12,13 +33,12 @@ class FetchDemo extends React.Component {
   }
 
   componentDidMount() {
-    axios.get('https://launchlibrary.net/1.2/launch/next/5')
+    axios.get('https://launchlibrary.net/1.2/launch/next/20')
       .then(res => {
         const launches = res.data.launches;
         console.log(launches);
         this.setState({ launches });
       });
-
   }
 
   render() {
@@ -26,13 +46,15 @@ class FetchDemo extends React.Component {
       <div className="app-wrapper">
         <h1 className="header">{this.props.headerText}</h1>
         {this.state.launches.map(launch =>
-          <div className="launch-wrapper" key={launch.id}>
+          <div className="launch-wrapper" key={launch.id}  onClick={() => openModal(launch.rocket.imageURL)}>
             <ul className="info-list">
               <li className="name">{launch.name}</li>
               <li className="start">{launch.windowstart}</li>
               {getVidUrl(launch.vidURLs)}
             </ul>
-            <img className="image" src={launch.rocket.imageURL} />
+            <a href="#" className="modal">
+              <img className="image" src={launch.rocket.imageURL} />
+            </a>
           </div>
         )}
       </div>
@@ -50,4 +72,12 @@ function getVidUrl(url_arr){
         return <li className="link"><a href={url_arr[0]}>Live stream</a></li>
     }
     return;
+}
+
+
+function openModal(url) {
+  console.log(url);
+
+  modal.setContent('<img src="' + url + '" />');
+  modal.open();
 }
