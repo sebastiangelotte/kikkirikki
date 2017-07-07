196b6642f6bb0ac4cd6bd36647b575e8015b63f0
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2017-07-08 01:57:11 +0200

    Small fixes

--- src/js/App.js
+++ src/js/App.js
@@ -39,6 +39,7 @@ const Item = styled.div`
   height: 300px;
   position: relative;
   overflow: hidden;
+  border: 10px solid #FFF;
   background-size: cover;
   background-repeat: no-repeat;
   background-position: top center;
@@ -48,10 +49,11 @@ const Item = styled.div`
   padding: 30px 40px;
   display: flex;
   flex-direction: column;
-  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  transition: all 0.3s cubic-bezier(.25,.8,.25,1);
 
   &:hover {
-
+    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
   }
 `
 
